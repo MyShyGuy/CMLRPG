c91416ea1186c169c9378621c807ea1979902777
MyShyGuy <matthias.schober.con@gmail.com>
2024-04-30 15:32:09 +0200

    Projektdateien hinzufügen.

---
+++ CMLRPG/Character/CharacterClass.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMLRPG.Character
+{
+    class CharacterClass
+    {
+        public string Name { get; private set; }
+        public string Archetyp { get; private set; }
+        public int Health { get; private set; }
+        public int MaxHealth { get; private set; }
+        public int MinHealth { get; private set; }
+        public int Attack { get; private set; }
+        public int Defense { get; private set; }
+
+
+
+        public CharacterClass() 
+        {
+            Name = "MrNobody";
+            Archetyp = "Noob";
+            Health = 100;
+            MaxHealth = 150;
+            MinHealth = 0;
+            Attack = 0;
+            Defense = 0;
+        }
+
+        public CharacterClass(string Name, string Archetyp, int Health, int MaxHealth, int MinHealth, int Attack, int Defense)
+        {
+            this.Name = Name;
+            this.Archetyp = Archetyp;
+            this.Health = Health;
+            this.MaxHealth = MaxHealth;
+            this.MinHealth = MinHealth;
+            this.Attack = Attack;
+            this.Defense = Defense;
+        }
+
+        public int AttackTarget()
+        {
+            return Attack;
+        }
+
+        public int DefendAgainstTarget()
+        {
+            int BlockAttack = Defense + 10;
+            return BlockAttack;
+        }
+
+        public void Run()
+        {
+            Health = Health - 10;
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("Du isst ein Brötchen aus deiner Tasche.");
+            Health += 10;
+        }
+    }
+    
+    class Warrior : CharacterClass
+    {
+        public Warrior(string Name) : base(Name, "Melee Fighter", 150, 200, 0, 25, 35)
+        {
+
+        }
+    }
+
+    class Archer : CharacterClass
+    {
+        public Archer(string Name) : base(Name, "Ranged Fighter", 100, 150, 0, 35, 25)
+        {
+
+        }
+    }
+}
---
+++ CMLRPG/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CMLRPG.Character;
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Warrior Spieler2 = new Warrior("MyShyGuy");
+        Console.WriteLine($"Name: {Spieler2.Name}");
+        Console.WriteLine($"Health: {Spieler2.Health}");
+        Console.WriteLine($"Attack: {Spieler2.Attack}");
+        Console.WriteLine($"Defense: {Spieler2.Defense}");
+        Console.WriteLine($"Typ: {Spieler2.Archetyp}");
+        Console.WriteLine("");
+
+        Console.WriteLine("Ziel wird angegriffen!");
+        Console.WriteLine($"Angriff macht:{Spieler2.AttackTarget()} dmg!");
+        Console.WriteLine("das ziel ist zu mächtig!!! renn lieber los!");
+        Console.WriteLine("du rennst los");
+        Spieler2.Run();
+        Console.WriteLine("du bist entkommen!");
+        Console.WriteLine($"dein Leben ist auf {Spieler2.Health}");
+        Spieler2.Eat();
+        Console.WriteLine($"dein Leben ist auf {Spieler2.Health}");
+
+    }
+}
